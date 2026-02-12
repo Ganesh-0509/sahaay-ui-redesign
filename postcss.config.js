752ca6a109c37e8fc9fd479d72247d60213af534
Ganesh-0509 <ganesh957kumar@gmail.com>
2026-02-12 14:24:29 +0530

    final

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
 export default {
-<<<<<<< HEAD
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
     },
-=======
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    
-  },
->>>>>>> b8231e9cee2e7e456d089ebd32ba32a15161466b
 };
